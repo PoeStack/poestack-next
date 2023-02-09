4cf625d58bb54e4bccc9e6e09ba8d83247dedb63
Jake TyCyn <jaketycyn@gmail.com>
2023-02-09 09:49:24 -0800

    fix - document not found Portal.tsx

--- src/components/portal.tsx
+++ src/components/portal.tsx
@@ -1,47 +1,24 @@
 import { useRef, useEffect, useState, ReactNode } from "react";
 import { createPortal } from "react-dom";
-//import styles from "./Overlay.module.css";
-
-interface PortalProps {
-  children: ReactNode;
-}
-
-// export const Portal = (props: any) => {
-//   const ref = useRef<Element | null>(null);
-//   const [mounted, setMounted] = useState(false);
-
-//   useEffect(() => {
-//     ref.current = document.querySelector<HTMLElement>("#portal");
-//     setMounted(true);
-//   }, []);
-
-//   return mounted && ref.current
-//     ? createPortal(<div>{props.children}</div>, ref.current)
-//     : null;
-// };
 
 interface Props {
   children: any;
 }
 
 export const Portal: React.FC<Props> = ({ children }) => {
-  const el = document.createElement("div");
-  const wrapper: React.RefObject<HTMLElement> = useRef(el);
+  const [mounted, setMounted] = useState(false);
+
+  //
 
   useEffect(() => {
-    const current = wrapper.current as HTMLElement;
-    current.setAttribute("id", "overlay");
-    document.body.appendChild(current);
+    setMounted(true);
 
-    return () => {
-      document.body.removeChild(current);
-    };
+    return () => setMounted(false);
   }, []);
 
-  if (!wrapper.current) {
-    return <>{null}</>;
-  }
-  return createPortal(children, wrapper.current);
+  return mounted
+    ? createPortal(children, document.querySelector("#portal") as HTMLElement)
+    : null;
 };
 
 export default Portal;
