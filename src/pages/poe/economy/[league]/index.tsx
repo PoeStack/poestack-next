4072315a97465be10a17f18f2af4cca3911521cd
tme321 <tme321@gmail.com>
2023-02-10 09:44:29 -0500

    Clean up the economy page in various ways.

--- src/pages/poe/economy/[league]/index.tsx
+++ src/pages/poe/economy/[league]/index.tsx
@@ -1,29 +1,23 @@
 import { useState, useEffect } from "react";
-import { gql, useQuery } from "@apollo/client";
-import HSparkline from "../../../../components/hsparkline";
-import StyledSelect from "../../../../components/styled-select";
-import Image from "next/image";
+import { useQuery } from "@apollo/client";
 import StyledCard from "../../../../components/styled-card";
 import StyledPaginate from "../../../../components/styled-paginate";
-import LeagueSelect from "../../../../components/league-select";
 import { useRouter } from "next/router";
-import Link from "next/link";
-import { GeneralUtils } from "../../../../utils/general-util";
-import _ from "lodash";
 import { ItemGroupValueTimeseriesSearchInput } from "../../../../__generated__/resolvers-types";
 import ItemGroupTagSelect from "../../../../components/item-group-tag-select";
-import {
-  ItemGroupValueTimeseries,
-  StashSnapshotItemGroupSummarySearchInput,
-} from "../../../../__generated__/resolvers-types";
+import { ItemGroupValueTimeseries } from "../../../../__generated__/resolvers-types";
 import { usePoeLeagueCtx } from "../../../../contexts/league-context";
-import CurrencyValueDisplay from "../../../../components/currency-value-display";
+import StyledLoadingIndicator from "../../../../components/styled-loading-indicator";
+import ItemsTable from "./items-table/items-table";
+import { ItemGroupsQuery } from "./queries";
 
 export default function Economy() {
   const router = useRouter();
+
   const { league: routerLeague, tag } = router.query;
 
   const { league, setLeague } = usePoeLeagueCtx();
+
   useEffect(() => {
     setLeague(routerLeague);
   }, [routerLeague, setLeague]);
@@ -42,32 +36,14 @@ export default function Economy() {
         sortDirection: null,
         itemGroupHashTags: [],
       },
-    });
+  });
+
   const [itemValueTimeseries, setItemValueTimeseries] = useState<
     ItemGroupValueTimeseries[]
   >([]);
-  const { refetch: refetchHistograms } = useQuery(
-    gql`
-      query Entries($search: ItemGroupValueTimeseriesSearchInput!) {
-        itemGroupValueTimeseriesSearch(search: $search) {
-          results {
-            series {
-              entries {
-                timestamp
-                value
-              }
-              type
-            }
-            itemGroup {
-              icon
-              displayName
-              key
-              hashString
-            }
-          }
-        }
-      }
-    `,
+
+  const { loading, refetch: refetchHistograms } = useQuery(
+    ItemGroupsQuery,
     {
       variables: {
         search: timeseriesSearch,
@@ -101,116 +77,41 @@ export default function Economy() {
     refetchHistograms();
   }, [refetchHistograms, timeseriesSearch]);
 
-  if (!itemValueTimeseries) {
-    return "loading...";
-  }
-
   return (
-    <>
-      <div>
-        <StyledCard title={"Economy"}>
-          <ItemGroupTagSelect
-            selected={tag}
-            league={league?.toString() ?? "Sanctum"}
-            onSelectChange={(e: string[]) => {
-              router.push({ query: { league: league, tag: e } });
-            }}
-          />
+    <div>
+      <StyledCard title={"Economy"}>
+        <ItemGroupTagSelect
+          selected={tag}
+          league={league?.toString() ?? "Sanctum"}
+          onSelectChange={(e: string[]) => {
+            router.push({ query: { league: league, tag: e } });
+          }}
+        />
 
-          <div className="min-h-[20rem]">
-            <table className="w-full">
-              <thead>
-                <tr className="w-full">
-                  <th></th>
-                  <th>Name</th>
-                  <th>History</th>
-                  <th>Listings</th>
-                  <th>Value</th>
-                </tr>
-              </thead>
-              <tbody>
-                {itemValueTimeseries!.map((groupSeries, index) => (
-                  <tr key={index} className="hover:text-skin-accent">
-                    <td>
-                      <Image
-                        src={groupSeries.itemGroup?.icon ?? ""}
-                        alt="icon"
-                        width="25"
-                        height="25"
-                      />
-                    </td>
+        {
+          loading?
+            <StyledLoadingIndicator/> :
+            <ItemsTable items={itemValueTimeseries} league={league} />
+        }
 
-                    <td>
-                      <Link
-                        href={`/poe/economy/${league}/item-group/${groupSeries?.itemGroup?.hashString}`}
-                      >
-                        {GeneralUtils.itemGroupToDisplayName(
-                          groupSeries.itemGroup!
-                        )}
-                      </Link>
-                    </td>
-                    <td>
-                      <HSparkline data={groupSeries.series} />
-                    </td>
-                    <td>
-                      {(() => {
-                        const recent = groupSeries.series?.find(
-                          (s) => s.type === "totalValidListings"
-                        );
-
-                        return (
-                          <>
-                            {
-                              recent?.entries?.[recent.entries.length - 1]
-                                ?.value
-                            }
-                          </>
-                        );
-                      })()}
-                    </td>
-                    <td>
-                      {(() => {
-                        const recent = groupSeries.series?.find(
-                          (s) => s.type === "p10"
-                        );
-
-                        return (
-                          <>
-                            <CurrencyValueDisplay
-                              valueChaos={
-                                recent?.entries?.[recent.entries.length - 1]
-                                  ?.value ?? 0
-                              }
-                            />
-                          </>
-                        );
-                      })()}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          <StyledPaginate
-            currentSkip={timeseriesSearch.itemGroupSearch?.skip!}
-            limit={timeseriesSearch.itemGroupSearch?.limit!}
-            onSelectionChange={function (skip: number, limit: number): void {
-              setTimeseriesSearch({
-                ...timeseriesSearch,
-                ...{
-                  itemGroupSearch: {
-                    ...timeseriesSearch.itemGroupSearch,
-                    skip: skip,
-                    limit: limit,
-                  },
+        <StyledPaginate
+          currentSkip={timeseriesSearch.itemGroupSearch?.skip!}
+          limit={timeseriesSearch.itemGroupSearch?.limit!}
+          onSelectionChange={function (skip: number, limit: number): void {
+            setTimeseriesSearch({
+              ...timeseriesSearch,
+              ...{
+                itemGroupSearch: {
+                  ...timeseriesSearch.itemGroupSearch,
+                  skip: skip,
+                  limit: limit,
                 },
-              });
-            }}
-            hasMore={true}
-          />
-        </StyledCard>
-      </div>
-    </>
+              },
+            });
+          }}
+          hasMore={true}
+        />
+      </StyledCard>
+    </div>
   );
 }
