0c352e650f62efad8e5c8fc832905cdddda42af9
Jake TyCyn <jaketycyn@gmail.com>
2023-02-10 13:46:26 -0800

    feat - themeChanger + themecontext

---
+++ src/components/theme-changer.tsx
@@ -0,0 +1,74 @@
+import { Fragment, useState, useEffect } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { useTheme } from "next-themes";
+
+export default function ThemeChanger() {
+  //const [theme, setTheme] = useState("Dark");
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex justify-center rounded-md bg-skin-primary px-4 py-2 text-sm font-medium text-skin-base shadow-sm hover:bg-skin-secondary-dark focus:outline-none w-24 ">
+          {theme}
+          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-24 origin-top-right rounded-md bg-slate-800 shadow-lg text-lg font-semibold text-center">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm  hover:bg-white hover:text-black"
+                  onClick={() => setTheme("Light")}
+                >
+                  Light
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm  hover:bg-black hover:text-white   hover:bg-skin-tertiary"
+                  onClick={() => setTheme("Dark")}
+                >
+                  Dark
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm hover:bg-red-800/80 hover:text-black"
+                  onClick={() => setTheme("Vaal")}
+                >
+                  Vaal
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
---
+++ src/contexts/theme-context.tsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const initalContext: {
+  theme: string;
+  setTheme: any;
+} = {
+  theme: "Dark",
+  setTheme: (theme: string) => {},
+};
+
+export const Profile_Themes = ["Dark", "Light", "Vaal"];
+
+export const ProfileThemeContext = createContext(initalContext);
+
+export function PoeStackThemeProvider({ children }) {
+  const [theme, setTheme] = useState(Profile_Themes[0]);
+
+  const value = {
+    theme: theme,
+    setTheme: setTheme,
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setTheme(localStorage.getItem("selected-theme") ?? Profile_Themes[0]);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme) {
+      localStorage.setItem("selected-theme", theme);
+    }
+  }, [theme]);
+
+  return (
+    <ProfileThemeContext.Provider value={value}>
+      {children}
+    </ProfileThemeContext.Provider>
+  );
+}
+
+export const useProfileThemeContext = () => useContext(ProfileThemeContext);
