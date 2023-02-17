1751ce7bc9b8a8262565501273345a32f93dba87
Jake TyCyn <jaketycyn@gmail.com>
2023-02-17 12:13:29 -0800

    fix - bulk export bg + opacity

--- src/components/create-bulk-listing-modal.tsx
+++ src/components/create-bulk-listing-modal.tsx
@@ -144,7 +144,7 @@ export default function CreateBulkListingModal({
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className="fixed inset-0 bg-black bg-opacity-50" />
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto text-content-base">
@@ -158,7 +158,7 @@ export default function CreateBulkListingModal({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-theme-color-1 p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-surface-primary p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-content-base"
