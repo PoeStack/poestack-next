ac2c23863f6effa2b56dfb21c7ecb1d19704a677
Jake TyCyn <jaketycyn@gmail.com>
2023-02-18 15:35:55 -0800

    feat - Basic FAQ setup

--- src/pages/poe/faq.tsx
+++ src/pages/poe/faq.tsx
@@ -1,4 +1,5 @@
 import StyledCard from "@components/styled-card";
+import Image from "next/image";
 import Link from "next/link";
 import { Disclosure } from "@headlessui/react";
 import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
@@ -21,13 +22,25 @@ const faqs = [
     answer:
       " Currently stock/quantity info is tracked and used for pricing if you enabled the settings.",
   },
+
   {
-    question: "4",
-    answer: "Lorem ipsum dolor sit amet.",
+    question: "What are p-values?",
+    answer:
+      "The p-values represent the chaos value percentiles of the listings tracked by the tool, ranging from p5 to p50. The lower p-values indicate the average price of the lowest percentage of listings after applying price fixing filters; for instance, p5 represents the average price of the lowest 5% of listings. These p-values are also monitored for stock brackets, meaning that the p5 of the 100-250 stock bracket is the average price of the bottom 5% of listings from sellers who have 100-250 of the item in question.",
+    answerCont:
+      "Through testing, it appears that using p5/7 can help undercut or sell items quickly, while p10 is the average selling price and p10+ may require more time to sell. The graphs display the price ranges between p-values and their movements, with a small range indicating a relatively stable price. For example, the essence shown in the graph has a 1c range for the entire range (difference between p20 and p7), which is small for a 23c item with 3k listings and thus provides a reasonable level of confidence in the displayed prices.",
+    image: "/PvalueExample.png",
   },
   {
-    question: "5",
-    answer: "Lorem ipsum dolor sit amet.",
+    question: "Why is the tool skipping my map/unique items stash tab(s)?",
+    answer:
+      "GGG considers every sub-tab as a distinct tab and has rate limits on the number of tabs that can be requested per minute/5 minute period. Currently, the tool does not index map stash tabs to avoid consuming all the stash requests and prolonging the snapshot process to around 15-25 minutes. Although we are searching for a solution, moving maps to a bulk tab and directing the tool to that location remains the most effective approach when selling maps.",
+  },
+  {
+    question:
+      "Will you consider making this tool open source or is it already open source?",
+    answer: " Currently the frontend is open source",
+    link: "https://github.com/zach-herridge/poestack-next",
   },
 ];
 
@@ -36,12 +49,12 @@ export default function FAQPage() {
     <>
       <StyledCard>
         <div className="">
-          <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:py-40 lg:px-8">
-            <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
+          <div className="mx-auto  max-w-8xl px-6 py-24 sm:py-32">
+            <div className="mx-auto max-w-4xl divide-y divide-content-base">
               <h2 className="text-2xl font-bold leading-10 tracking-tight text-content-base">
                 Frequently asked questions
               </h2>
-              <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
+              <dl className="mt-4 space-y-6 divide-y divide-color-base">
                 {faqs.map((faq) => (
                   <Disclosure as="div" key={faq.question} className="pt-6">
                     {({ open }) => (
@@ -66,8 +79,11 @@ export default function FAQPage() {
                             </span>
                           </Disclosure.Button>
                         </dt>
-                        <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                          <p className="text-base leading-7 text-content-base">
+                        <Disclosure.Panel
+                          as="dd"
+                          className="mt-2 p-4 bg-surface-primary-variant rounded-md"
+                        >
+                          <p className="text-content-base leading-7">
                             {faq.answer}{" "}
                             {faq.link ? (
                               <Link href={faq.link} className="" legacyBehavior>
@@ -79,6 +95,24 @@ export default function FAQPage() {
                                   here
                                 </a>
                               </Link>
+                            ) : null}
+                            {faq.answerCont ? (
+                              <div>
+                                <br />
+                                {faq.answerCont}
+                              </div>
+                            ) : null}
+                            {faq.image ? (
+                              <div className="flex flex-row justify-center">
+                                <Image
+                                  width={1800}
+                                  height={1200}
+                                  object-fit="contain"
+                                  src={faq.image}
+                                  alt="image"
+                                  quality={100}
+                                />
+                              </div>
                             ) : null}
                           </p>
                         </Disclosure.Panel>
@@ -100,22 +134,7 @@ export default function FAQPage() {
 //         <div className=" text-center">
 //           <StyledCard title="FAQ">
 //             <ul className="space-y-4 text-left">
-//               <li>
-//        >
-//               </li>
-//               <li>
-//                 <h1 className="text-xl mb-6">
-//                   Does this tool take stock/quantity info into account?
-//                 </h1>
-//                 <p>
-
-//                 </p>
-//               </li>
-//               <li>
-//                 <h1 className="text-xl mb-6">
-//                   Will you consider making this tool open source or is it
-//                   already open source?
-//                 </h1>
+//
 //                 <p>
 //                   Currently the frontend is open source{" "}
 //                   <Link
