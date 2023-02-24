6cb55760d5262e6f45caa9627466aaf4985a06e0
Jake TyCyn <jaketycyn@gmail.com>
2023-02-24 01:10:58 -0800

    Feat - Tutorial Page

---
+++ src/pages/poe/tutorial.tsx
@@ -0,0 +1,18 @@
+export default function TutorialPage() {
+  return (
+    <>
+      <div className="flex flex-col justify-center mx-auto space-y-4 h-96">
+        <h1 className="text-left">Coming Soon</h1>
+        <h1 className="text-left">Check FAQ in footer for now</h1>
+        <h4 className="text-left">
+          In the future will include youtube embeds to any videos made.
+        </h4>
+        <h6 className="text-left">
+          {" "}
+          Please post in the discord any helpful tips or information you would
+          like to pass along to the rest of the community!
+        </h6>
+      </div>
+    </>
+  );
+}
