699918ed0c348e2f4c9f8f891e6fe2e3d143e96e
Jake TyCyn <jaketycyn@gmail.com>
2023-04-06 10:56:49 -0700

    added spinning

--- src/components/styled-loading.tsx
+++ src/components/styled-loading.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function StyledLoading() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen mx-auto">
+      <div className="flex items-center justify-center h-screen mx-auto animate-spin">
         <Image src={"/KEKW.png"} height={48} width={48} alt={"Kekw"}></Image>
       </div>
     </>
