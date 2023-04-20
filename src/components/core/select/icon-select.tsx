a00c92388f645eb43400d6a2a038a8a7fd974534
João Victor <joaovictorvisona@gmail.com>
2023-04-19 23:42:13 -0300

    select with icon component

---
+++ src/components/core/select/icon-select.tsx
@@ -0,0 +1,50 @@
+import { SetOptional } from "type-fest";
+import clsx from "clsx";
+import Image from "next/image";
+
+import {
+  CustomSelect,
+  CustomSelectProps,
+} from "@components/core/select/custom-select";
+import { SELECT_PLACEHOLDER } from "@components/core/select/utils";
+
+interface IconItem {
+  name?: string;
+  icon?: string;
+}
+
+export type IconSelectProps<T extends IconItem> = SetOptional<
+  CustomSelectProps<T>,
+  "renderer" | "keyGenerator"
+>;
+
+export function IconSelect<T extends IconItem>(props: IconSelectProps<T>) {
+  return (
+    <CustomSelect<T>
+      {...props}
+      renderer={({ data, selected }) => (
+        <div className="flex items-center">
+          <span
+            className={clsx(
+              selected ? "font-semibold text-content-accent" : "font-normal",
+              "ml-3 block text-base"
+            )}
+          >
+            <div className="flex space-x-2">
+              {data?.icon ? (
+                <Image
+                  width={24}
+                  height={24}
+                  src={data.icon}
+                  alt={`${data?.name} icon`}
+                />
+              ) : null}
+              <p>{data?.name ?? SELECT_PLACEHOLDER}</p>
+            </div>
+          </span>
+        </div>
+      )}
+      keyGenerator={(x) => x?.name || "unknown"}
+    />
+  );
+}
--- src/components/core/select/index.tsx
+++ src/components/core/select/index.tsx
@@ -6,3 +6,5 @@ export default Select;
 
 export { CustomSelect } from "@components/core/select/custom-select";
 export { DefaultOption } from "@components/core/select/default-option";
+
+export { IconSelect } from "@components/core/select/icon-select";
