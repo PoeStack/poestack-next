04d9c9702461901d64e69a1c49179cf60d0ec6ff
Zach Herridge <zgherridge@gmail.com>
2023-09-13 01:17:31 -0700

    remove bottom ad

--- src/components/ads/styled-square-responsive-ad.tsx
+++ src/components/ads/styled-square-responsive-ad.tsx
@@ -23,7 +23,7 @@ export default function FixedAds() {
   return (
     <>
       <div key={`key_${key}`}>
-        <div className="fixed z-50 top-[15%] right-0 w-[180px] h-[500px]">
+        <div className="fixed top-[15%] right-0 z-50 h-[500px] w-[180px]">
           <Adsense
             client="ca-pub-1917075558725173"
             slot="4469612839"
@@ -32,19 +32,10 @@ export default function FixedAds() {
             responsive="true"
           />
         </div>
-        <div className="fixed z-50 top-[15%] left-0 w-[180px] h-[500px]">
+        <div className="fixed top-[15%] left-0 z-50 h-[500px] w-[180px]">
           <Adsense
             client="ca-pub-1917075558725173"
             slot="4469612839"
-            style={{ display: "block" }}
-            format="auto"
-            responsive="true"
-          />
-        </div>
-        <div className="fixed z-50 bottom-0 right-0 w-[500px] h-[500px] ">
-          <Adsense
-            client="ca-pub-1917075558725173"
-            slot="4522170456"
             style={{ display: "block" }}
             format="auto"
             responsive="true"
